af501535c99d6a567cb1ef1a3a0abc679d732897
Daniel Cordero <oak@0xdc.host>
2016-11-28 11:52:29 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+while getopts c:vKs opt; do
+	case $opt in
+	c)	transport="-c $OPTARG"
+		;;
+	K|s|v)	exargs="$exargs -$opt"
+		;;
+	esac
+done
+
+shift $((OPTIND-1))
 hosts=$1
 shift
 
 playbook=$(mktemp -p .)
 echo -e "---\n- hosts: $hosts\n  roles:" > $playbook
 for role in $@; do echo "  - $role"; done >> $playbook
-ansible-playbook -v $playbook
+ansible-playbook $exargs $transport $playbook
 rm "$playbook"
